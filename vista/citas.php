338f85fdbd188d5398d7b5cc484f2311cd806090
José Antonio Macho García <aguila.elenita@gmail.com>
2024-07-10 19:09:22 +0200

    se muestran las citas en tabla, if por si no hay citas

--- vista/citas.php
+++ vista/citas.php
@@ -6,70 +6,88 @@ include("../modelo/Doctrine/bootstrap.php");
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RegistroUsuario</title>
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <link href="vista/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/Peluqueria/vista/css/headers.css">
-    <link rel="stylesheet" href="/Peluqueria/vista/css/registroUsuario.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RegistroUsuario</title>
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+  <link href="vista/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/Peluqueria/vista/css/headers.css">
+  <link rel="stylesheet" href="/Peluqueria/vista/css/registroUsuario.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-<div class="bd-example-snippet bd-code-snippet">
-  <div class="bd-example m-0 border-0">
-    
-        <table class="table table-striped">
-          <thead>
+  <div class="bd-example-snippet bd-code-snippet">
+    <?php
+    //obtenemos el usuario
+    $usuario = $entityManager->getRepository("usuario")
+      ->findBy(array('idUsuario' => $_SESSION['idUsuario']));
+    //obtenemos solo el id del usuario
+    $idUsuario = $usuario[0]->getIdUsuario();
+    //obtenemos las citas con el id del usuario actual y ordenamos en orden ascendente
+    $citas = $entityManager->getRepository("citas")
+      ->findBy(array('usuario' => $idUsuario), ['fecha_cita' => 'ASC']);
+    if (!empty($citas)) {
+     
+    ?>
+    <div class="bd-example m-0 border-0">
+
+      <table class="table table-striped">
+        <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">idCita</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Estado</th>
           </tr>
-          </thead>
+        </thead>
 
-          <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-          </tbody>
-        </table>
-        
-  </div>
-</div>
+        <tbody>
+          <?php
+          // contador para llevar una cuenta de citas en la tabla
+          $contador = 1;
+          foreach ($citas as $cita) {
 
+          ?>
+            <tr>
+              <th scope="row"><?php echo $contador ?></th>
+              <td><?php echo $cita->getIdCitas(); ?></td>
+              <td><?php echo $cita->getFecha_cita()->format('d-m-y'); ?></td>
+              <?php
+              // estructura switch que cambia el color del estado dependiendo del estado en el 
+              // que se encuentre
+              switch ($cita->getEstado()) {
+                case 'pendiente':
+                  echo "<td style='color: orange;'>" . $cita->getEstado() . "</td>";
+                  break;
+                case 'aceptada':
+                  echo "<td style='color: green;'>" . $cita->getEstado() . "</td>";
+                  break;
+                case 'rechazada':
+                  echo "<td style='color: red;'>" . $cita->getEstado() . "</td>";
+                  break;
+              }
+              ?>
+            </tr>
+          <?php
+            $contador += 1;
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
     <?php
-           //obtenemos el usuario
-           $usuario = $entityManager->getRepository("usuario")
-           ->findBy(array('idUsuario' => $_SESSION['idUsuario']));
-           //obtenemos solo el id del usuario
-           $idUsuario = $usuario[0]->getIdUsuario();
-           //obtenemos las citas con el id del usuario actual
-           $citas = $entityManager->getRepository("citas")
-           ->findBy(array('usuario' => $idUsuario));
-           print_r($citas);
+     }
+     else {
+      echo "<div class='d-flex justify-content-center align-items-center' style='height: 100vh;'><h1>No hay citas</h1></div>";
+     }
     ?>
-    <script src="/Peluqueria/vista/js/header.js"></script>
-    <script src="/Peluqueria/vista/js/login.js"></script>
-    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+  </div>
+  <script src="/Peluqueria/vista/js/header.js"></script>
+  <script src="/Peluqueria/vista/js/login.js"></script>
+  <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 
 </html>
